76248f7970903661518392a0f0bbfa1536154be5
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-01-31 11:58:52 +0100

    Write some documentation on vote rate

--- src/voterate.jl
+++ src/voterate.jl
@@ -1,7 +1,14 @@
+# ATTENTION:
+# - attention is a number (here: Float64)
+# - require protocol implementor to provide attention operationalized as a number
+# - each implementor has to operationalize attention themselves, but we can provide a default
+#
+# VOTERATE:
+# - also a number (here: Float64)
+# - number of votes over the total attention on the post
+
+
 function calc_voterate(tally::SimpleTally, attention::Float64)
   return update(GLOBAL_PRIOR_UPVOTE_PROBABILITY, SimpleTally(tally.total, attention)).mean
 end
-
-# attention is a number (here: Float64)
-# so require protocol implementor to provide attention operationalized as a number
 
