edf8678a59324233c6dfbecec412827ae17ab521
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2024-02-07 15:03:28 +0100

    Adjust simple test and add agent simulation script

--- scripts/simple-test.jl
+++ scripts/simple-test.jl
@@ -1,5 +1,13 @@
 using GlobalBrain
 
+# --- Discussion tree:
+# 1
+# |-2
+#   |-4
+#   |-5
+# |-3
+#   |-6
+#     |-7
 posts = [
   Post(1, nothing),
   Post(2, 1),
@@ -7,10 +15,10 @@ posts = [
   Post(4, 2),
   Post(5, 2),
   Post(6, 3),
-  Post(7, 3),
+  Post(7, 6),
 ]
 
-post_tally = BernoulliTally(26, 51)
+# post_tally = BernoulliTally(26, 51)
 
 informed_tallies_vec = [
   InformedTally(1, 2, BernoulliTally(5, 10), BernoulliTally(8, 14), BernoulliTally(6, 9)),
@@ -18,7 +26,7 @@ informed_tallies_vec = [
   InformedTally(2, 4, BernoulliTally(12, 12), BernoulliTally(8, 9), BernoulliTally(0, 10)),
   InformedTally(2, 5, BernoulliTally(5, 7), BernoulliTally(5, 6), BernoulliTally(7, 13)),
   InformedTally(3, 6, BernoulliTally(3, 4), BernoulliTally(5, 8), BernoulliTally(2, 3)),
-  InformedTally(3, 7, BernoulliTally(8, 15), BernoulliTally(7, 10), BernoulliTally(3, 15)),
+  InformedTally(6, 7, BernoulliTally(8, 15), BernoulliTally(7, 10), BernoulliTally(3, 15)),
 ]
 
 informed_tallies = Dict(p.id => InformedTally[] for p in posts)
@@ -28,31 +36,22 @@ for it in informed_tallies_vec
   end
 end
 
-println("Starting computation...")
-estimate = score_thread(1, post_tally, informed_tallies)
-
 println("--------------")
 println("Informed tallies: ")
 for k in sort(collect(keys(informed_tallies)))
-  println(k, ": ", informed_tallies[k])
+  println(k, ":")
+  for it in informed_tallies[k]
+    println(it)
+  end
 end
+
 println("--------------")
-println("Results: ", estimate)
-# println("top_note_id: ", estimate.note_id)
-# println("given shown note: ", estimate.p_given_shown_note)
-# println("given not shown note: ", estimate.p_given_not_shown_note)
+println("Starting computation...")
+estimate = score_thread(1, informed_tallies)
 
-
-# ------------------------------------------------------------------------------
-# --- OLD EXAMPLE --------------------------------------------------------------
-# ------------------------------------------------------------------------------
-
-# --- Discussion tree:
-# 1
-# |-2
-#   |-4
-#   |-5
-# |-3
-#   |-6
-#   |-7
+println("--------------")
+println("Results: ")
+for e in estimate
+  println(e)
+end
 
