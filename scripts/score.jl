3c9054ebe01899a2fe97e15b662bd96d72bef71a
Jonathan R. Warden <john.warden@gmail.com>
2024-02-10 17:18:33 +0100

    Update scoring formula

--- scripts/score.jl
+++ scripts/score.jl
@@ -5,7 +5,7 @@ prior = GLOBAL_PRIOR_UPVOTE_PROBABILITY
 
 uninformedTally = BernoulliTally(1,1)
 informedTally = BernoulliTally(0,1)
-deltaVotes = 1
+# deltaVotes = 1
 
 q = update(prior, uninformedTally)
 
@@ -22,82 +22,70 @@ bayesian_avg(prior, uninformedTally+informedTally)
 # uninformedTally = The tally of users who have not been exposed to note
 # informedTally = The tally after user have been exposed to note
 
-function incremental_information_gain(uninformedTally::BernoulliTally, informedTally::BernoulliTally, deltaVotes::Int)
-
-	@assert(uninformedTally.sample_size >= 1, "uninformedTally.sample_size < 1")
+function score(informedTally::BernoulliTally, uninformedTally::BernoulliTally, selfTally::BernoulliTally)
 
 
 	q = update(prior, uninformedTally)
-	# qAgain = uninformedTally.count/uninformedTally.sample_size
-
 	p = update(reset_weight(q, WEIGHT_CONSTANT), informedTally)
 
-	# Calculate the cognitive dissonance among uninformed users only
-	n = uninformedTally.sample_size
-	upvotes = uninformedTally.count
+	pSelf = update(prior, selfTally)
 
 
-	dkl = relative_entropy(p.mean, q.mean)
-	cogdiss = n * dkl
-
-	# Now hypothetical situation given submitter is shown note
-	# Consider the same set of n users. Calculate their average belief qNew
-	# uninformedTallyNew = BernoulliTally(upvotes-deltaVotes,n)
-	uninformedTallyNew = uninformedTally + (-deltaVotes,0)
-
-	qNew = update(prior, uninformedTallyNew)
-
-
-	dklNew = relative_entropy(p.mean, qNew.mean)
-	cogdissNew = n * dklNew
-
-	# Same result
-	# cogdiss - cogdissNew
-
-	# Again Same result
-	# n * (
-	# 	relative_entropy(p, q)
-	# 	- relative_entropy(p, qNew)
-	# )
-
-
-	# Formula from https://social-protocols.org/global-brain/information-value.html
-	# that formula multiplies by votesTotal, where we multiply by which is the number of unformed votes
-	# total_information_gain = n * information_gain(p, q, qNew)
-	# TODO: This should be our final scoring function I think
-	# return
-		# upvote_probability_note * (1 + log(upvote_probability_note))
-		# + information_gain(upvote_probability_post, uninformed_upvote_probability_post, qNew)
-
-
-
-	# make a datafram with each of these values
-	return DataFrame(
-		p = p,
-		q = q,
-		upvotes = uninformedTally.count + informedTally.count,
-		n = n,
-		dkl = dkl,
-		cogdiss = cogdiss,
-		# pNew = betaMean(pNew),
-		qNew = qNew,
-		# nNew = nNew,
-		# upvotesNew = uninformedTallyNew.count + informedTallyNew.count,
-		dklNew = dklNew,
-		cogdissNew = cogdissNew,
-		gain = information_gain
-	)
+	return score(sample(p), sample(q), sample(pSelf))
 
 end
 
 
 
+# The score for a post is calculated based on three values:
+# p: the informed probability for parent of this post (given considered this note)
+# q: the uninformed probability for parent of this post (given not considered this note)
+# pSelf is informed upvoteProbability for current post
+function score(p::BetaDistribution, q::BetaDistribution, pSelf::BetaDistribution)
+
+
+
+	@assert(uninformedTally.sample_size >= 1, "uninformedTally.sample_size < 1")
+
+	# so q is a tally of people who ARE uninformed OR WHO VOTED BEFORE BEING INFORMED.
+	# total cognitive dissonance is just total relative entropy
+	# q isn't expected to  change as people change votes as described in: https://social-protocols.org/global-brain/information-value.html
+	# This doc should be updated.
+	# The probabiliy of a changed vote does't change. It's still (p-q) / q.
+
+
+	dkl = relative_entropy(p, q)
+
+	return
+		pSelf * (1 + log(pSelf))
+		+ dkl
+
+
+	# make a datafram with each of these values
+	return DataFrame(
+		p = p.mean,
+		q = q.mean,
+		upvotes = uninformedTally.count + informedTally.count,
+		n = n,
+		dkl = dkl,
+		# cogdiss = cogdiss,
+		# pNew = betaMean(pNew),
+		# qNew = qNew,
+		# nNew = nNew,
+		# upvotesNew = uninformedTallyNew.count + informedTallyNew.count,
+		# dklNew = dklNew,
+		# cogdissNew = cogdissNew,
+		# gain = total_information_gain
+	)
+
+end
+
 
 # uninformedTally = BernoulliTally(2,2)
 # informedTally = BernoulliTally(0,1)
 
-df1 = incremental_information_gain(BernoulliTally(1,1), BernoulliTally(0,1), 1)
-df2 = incremental_information_gain(BernoulliTally(2,2), BernoulliTally(0,1), 1)
+df1 = incremental_information_gain(BernoulliTally(1,1), BernoulliTally(0,1))
+df2 = incremental_information_gain(BernoulliTally(2,2), BernoulliTally(0,1))
 
 
 # Compare this to the detailed example https://social-protocols.org/global-brain/cognitive-dissonance.html#detailed-example
@@ -105,7 +93,7 @@ df2 = incremental_information_gain(BernoulliTally(2,2), BernoulliTally(0,1), 1)
 # uninformedTally = BernoulliTally(900,1000)
 # informedTally =BernoulliTally(100,500)
 
-df3 = incremental_information_gain(BernoulliTally(900,1000), BernoulliTally(100,500), 70)
+df3 = incremental_information_gain(BernoulliTally(900,1000), BernoulliTally(100,500))
 
 df = vcat(df1, df2, df3)
 
