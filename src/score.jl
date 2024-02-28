4adfab0b9d1dcce28ac437e37708ab1a5e773a21
Jonathan R. Warden <john.warden@gmail.com>
2024-02-28 17:31:37 +0100

    Fix bug in parentP and parentQ calculation

--- src/score.jl
+++ src/score.jl
@@ -3,7 +3,7 @@ function score(score_data::ScoreData)::Float64
 	parentP = parent_informed_probability(score_data)
 	parentQ = parent_uninformed_probability(score_data)
 
-	return p*(1 + log2(p)) + relative_entropy(parentP, parentQ)
+	return p*(1 + log2(p)) + (isnothing(score_data.effect) ? 0 : relative_entropy(parentP, parentQ))
 end
 
 function informed_probability(score_data::ScoreData)::Float64 
@@ -14,12 +14,12 @@ function uninformed_probability(score_data::ScoreData)::Float64
 	return isnothing(score_data.top_note_effect) ? score_data.self_probability : score_data.top_note_effect.uninformed_probability
 end
 
-function parent_informed_probability(score_data::ScoreData)::Float64 
-	return isnothing(score_data.effect) ? score_data.self_probability : score_data.effect.informed_probability
+function parent_informed_probability(score_data::ScoreData)::Union{Float64,Nothing} 
+	return isnothing(score_data.effect) ? nothing : score_data.effect.informed_probability
 end
 
-function parent_uninformed_probability(score_data::ScoreData)::Float64 
-	return isnothing(score_data.effect) ? score_data.self_probability : score_data.effect.uninformed_probability
+function parent_uninformed_probability(score_data::ScoreData)::Union{Float64,Nothing} 
+	return isnothing(score_data.effect) ? nothing : score_data.effect.uninformed_probability
 end
 
 function top_note_id(score_data::ScoreData)::Union{Int64,Nothing} 
